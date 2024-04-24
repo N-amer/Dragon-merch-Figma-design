6ac1b0e17de2fba522586d81de0cf489d70664b2
Nooman Amer <nooman178@gmail.com>
2024-04-24 13:52:54 +0200

    added side menu animation

--- Code/script.js
+++ Code/script.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll("nav .nav-item a");
-  const navLinksMobile = document.querySelectorAll("#navbar-menu .nav-item a");
+  const navLinksMobile = document.querySelectorAll("#side-menu a");
   const navItems = document.querySelectorAll(".nav-item");
   const burgerMenuClosed = document.getElementById("burger-menu-closed");
   const burgerMenuOpened = document.getElementById("burger-menu-opened");
   const sideMenu = document.getElementById("side-menu");
 
   burgerMenuClosed.addEventListener("click", () => {
-    sideMenu.classList.remove("hidden");
-    sideMenu.classList.toggle("block");
+    sideMenu.classList.remove("translate-x-full");
+    sideMenu.classList.add("translate-x-0");
   });
 
   burgerMenuOpened.addEventListener("click", () => {
-    sideMenu.classList.toggle("hidden");
-    sideMenu.classList.remove("block");
+   sideMenu.classList.remove("translate-x-0");
+    sideMenu.classList.add("translate-x-full");
   });
 
   // navbar navigations
@@ -47,6 +47,10 @@ document.addEventListener("DOMContentLoaded", function () {
         item.classList.remove("active");
       });
       this.closest(".nav-item").classList.add("active");
+
+      // menu closes after clicking on a link
+      sideMenu.classList.remove("block");
+      sideMenu.classList.add("hidden");
     });
   });
 });
