239e1bdfad597618c5aa05e76245c09f2cf10f98
Nooman Amer <nooman178@gmail.com>
2024-04-24 13:22:48 +0200

    added side menu and fixed most navbar responsiveness

--- Code/script.js
+++ Code/script.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll("nav .nav-item a");
+  const navLinksMobile = document.querySelectorAll("#navbar-menu .nav-item a");
   const navItems = document.querySelectorAll(".nav-item");
-  const burgerMenu = document.getElementById('burger-menu');
-  const sideMenu = document.getElementById('side-menu');
+  const burgerMenuClosed = document.getElementById("burger-menu-closed");
+  const burgerMenuOpened = document.getElementById("burger-menu-opened");
+  const sideMenu = document.getElementById("side-menu");
 
-  burgerMenu.addEventListener('click', () => {
-    sideMenu.classList.toggle('translate-x-0');
+  burgerMenuClosed.addEventListener("click", () => {
+    sideMenu.classList.remove("hidden");
+    sideMenu.classList.toggle("block");
   });
 
+  burgerMenuOpened.addEventListener("click", () => {
+    sideMenu.classList.toggle("hidden");
+    sideMenu.classList.remove("block");
+  });
+
+  // navbar navigations
   navLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
       event.preventDefault();
@@ -20,7 +29,24 @@ document.addEventListener("DOMContentLoaded", function () {
       navItems.forEach((item) => {
         item.classList.remove("active");
       });
-      this.closest('.nav-item').classList.add('active');
+      this.closest(".nav-item").classList.add("active");
+    });
+  });
+
+  // menu navigations
+  navLinksMobile.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      targetSection.scrollIntoView({ behavior: "smooth" });
+
+      navItems.forEach((item) => {
+        item.classList.remove("active");
+      });
+      this.closest(".nav-item").classList.add("active");
     });
   });
 });
